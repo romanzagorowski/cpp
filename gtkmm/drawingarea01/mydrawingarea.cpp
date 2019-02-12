2e7b223928ac55b16552d843bad4b6422f5ba3fe
roman <roman.zagorowski@hotmail.com>
2019-02-12 15:40:25 +0100

    on_draw count added

--- gtkmm/drawingarea01/mydrawingarea.cpp
+++ gtkmm/drawingarea01/mydrawingarea.cpp
@@ -3,12 +3,14 @@
 
 bool MyDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
 {
+    static int count = 0;
+
     Gtk::Allocation a = this->get_allocation();
 
     const int w = a.get_width();
     const int h = a.get_height();
 
-    std::cout << "on_draw: width=" << w << ", height=" << h << std::endl;
+    std::cout << "on_draw (" << ++count << "): width=" << w << ", height=" << h << std::endl;
 
     cr->set_source_rgb(0.0, 0.0, 0.0);
     cr->paint();
